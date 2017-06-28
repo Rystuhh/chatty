a61b9cb894988cc79fae3d273bfa9730177f3e67
Rystuhh <ryguy12290@gmail.com>
2017-06-27 20:31:02 -0600

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+app.listen(3000);
+
+var messages = [];
+
+app.get('/messages', function (req, res, next) {
+  res.status(200).json({messsages: messages});
+});
+
+app.post('/messages', function (req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({messages: messages});
+});
